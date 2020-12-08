d0bb687ab277a5883c3901a6567e509fc54935fa
reeya-tamrakar <reeya6792@gmail.com>
2020-12-08 20:42:08 +0545

    templates file added.

--- shop/views.py
+++ shop/views.py
@@ -5,5 +5,9 @@ def index(request):
     return HttpResponse('Shop Homepage<br><a href="/shop/profile">Go to '
                         'profile!</a>')
 
+def store(request):
+    context = {}
+    return render(request)
+
 def profile(request):
     return HttpResponse('Shop Profilepage <a href="/shop">Back</a>')
