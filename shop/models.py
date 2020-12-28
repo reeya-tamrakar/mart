e6f38e9213279b70e8db607276cee60a2a2223fb
Asis <45727645+praash47@users.noreply.github.com>
2020-12-28 20:43:06 +0545

    preparing for media upload and minor fixes.

--- shop/models.py
+++ shop/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    price = models.FloatField()
+    img1 = models.ImageField(upload_to="shop/products")
     in_stock = models.BooleanField(default=True)
 
     def __str__(self):
