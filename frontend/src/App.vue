476d4bd330cd2ec1c9e418054bc5bd62321a83e8
Asis <45727645+praash47@users.noreply.github.com>
2020-12-30 21:03:37 +0545

    axios fetch and vue added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,55 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">Content</router-link>
-  </div>
+    <header class="header shop">
+    <!-- Header Inner -->
+    <div class="header-inner">
+      <div class="container">
+          <div class="cat-nav-head">
+              <div class="row">
+                  <div class="col-lg-9 col-12">
+                      <div class="menu-area">
+                          <!-- Main Menu -->
+                          <nav class="navbar navbar-expand-lg">
+                              <div class="navbar-collapse">
+                                  <div class="nav-inner">
+                                      <ul class="nav main-menu menu navbar-nav">
+                                              <li><router-link to="/">Home</router-link></li>
+                                              <li><router-link to="product">Product</router-link></li>
+                                              <li><router-link to="service">Service</router-link></li>
+                                              <li><router-link to="shop">Shop<i class="ti-angle-down"></i><span class="new">New</span></router-link>
+                                                  <ul class="dropdown">
+                                                      <li><router-link to="shop-grid">Shop Grid</router-link></li>
+                                                      <li><router-link to="cart">Cart</router-link></li>
+                                                      <li><router-link to="checkout">Checkout</router-link></li>
+                                                  </ul>
+                                              </li>
+                                              <li><router-link to="pages">Pages</router-link></li>
+                                              <li><router-link to="blog">Blog<i class="ti-angle-down"></i></router-link>
+                                                      <ul class="dropdown">
+                                                          <li><router-link to="blog-single-sidebar">Blog Single Sidebar</router-link></li>
+                                                      </ul>
+                                                  </li>
+                                                  <li><router-link to="contact">Contact Us</router-link></li>
+                                              </ul>
+                                      </div>
+                                  </div>
+                              </nav>
+                              <!--/ End Main Menu -->
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </div>
+        <!--/ End Header Inner -->
+    </header>
   <router-view/>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.router-link-active {
+ color:#fff;
+ background:#F7941D;
 }
 </style>
--- shop/models.py
+++ shop/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    description = models.CharField(max_length=1000)
+    description = models.TextField()
     price = models.FloatField()
     img1 = models.ImageField(upload_to="shop/products")
     in_stock = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
+
+class ContactResponse(models.Model):
+    name = models.CharField(max_length=200)
