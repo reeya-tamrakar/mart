900d3c7960de5cff21109fab4d0da429b2053d75
praash47 <praash47@gmail.com>
2021-02-25 13:52:26 +0545

    final push to mart

--- frontend/src/views/Product.vue
+++ frontend/src/views/Product.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
-     <p>Hey!</p>
+  <div class="container m-4">
+    <!-- main product card -->
+    <div class="card">
+      <div class="card-body">
+        <h1 class="card-title">Water Bottle</h1>
+
+        <div class="row">
+          <!-- product image section -->
+          <div class="col-md-6 col-sm-12">
+            <img src="https://via.placeholder.com/768x768" alt="water bottle image">
+          </div>
+
+          <!-- product description section -->
+          <div class="col-md-6 col-sm-12">
+            <span class="price">Rs. 500</strong></span><br>
+            <h2>Description</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque odit labore fuga iste eius doloremque
+              laborum magnam fugit earum assumenda,voluptate doloribus provident id. Deleniti voluptas illo amet
+              adipisci repellat.</p><br>
+            <p><strong>In stock:</strong> 40</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<style scoped>
+.price {
+  font-size: 3rem;
+  margin-bottom: 100px;
+}
+</style>
