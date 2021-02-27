8a67d060bfc4ab6cffca7f1da9972391fd282af9
praash47 <praash47@gmail.com>
2021-02-27 20:52:58 +0545

    product and cart

--- frontend/src/views/Product.vue
+++ frontend/src/views/Product.vue
@@ -8,17 +8,23 @@
         <div class="row">
           <!-- product image section -->
           <div class="col-md-6 col-sm-12">
-            <img src="https://via.placeholder.com/768x768" alt="water bottle image">
+            <img src="https://via.placeholder.com/768" alt="water bottle image">
           </div>
 
           <!-- product description section -->
           <div class="col-md-6 col-sm-12">
-            <span class="price">Rs. 500</strong></span><br>
+            <span class="price">Rs. 1,24,500</span>
             <h2>Description</h2>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque odit labore fuga iste eius doloremque
               laborum magnam fugit earum assumenda,voluptate doloribus provident id. Deleniti voluptas illo amet
               adipisci repellat.</p><br>
             <p><strong>In stock:</strong> 40</p>
+            <span class="order-section">
+              <form id="order">
+                <label>Qty:</label><input type="number" name="quantity">
+                <button type="submit" class="submit">Order</button>
+              </form>
+            </span>
           </div>
         </div>
       </div>
@@ -26,8 +32,23 @@
   </div>
 </template>
 <style scoped>
-.price {
-  font-size: 3rem;
-  margin-bottom: 100px;
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+.price, .order-section {
+  display: block;
+  margin: 50px 0;
+}
+.price {font-size: 3rem;}
+.order-section input {
+  width: 50px;
+  margin-right: 20px;
+  margin-left: 10px;
+}
+.order-section .submit {
+  width: 100px;
+  height: 40px;
+  background: #2ECC40;
+  color: #fff;
 }
 </style>
